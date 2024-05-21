3e85ab36aa86f0f25b1d368055a869b37aad907c
Allen Taylor <allen.taylor0625@gmail.com>
2024-05-20 20:15:40 -0500

    Solved code challenge

---
+++ CodeChallenges/Multiples3sAnd5s/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Multiples3sAnd5s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code Challenge 1");
+
+            int sum = 0;
+            for (int currentNbr = 1; currentNbr < 1000; currentNbr++)
+            {
+                if (currentNbr % 3 == 0 || currentNbr % 5 == 0)
+                { 
+                    sum += currentNbr;
+                }
+            }
+            Console.WriteLine($"The sum of all multiples of 3 or 5 below 1000 is: {sum}");
+        }
+    }
+}
